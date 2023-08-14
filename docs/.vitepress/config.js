1522775555274c858e4dcbd6fb075782739d545a
htnanako <794046218@qq.com>
2023-08-14 14:21:08 +0800

    add config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,49 @@
 module.exports = {
-  title: "Hello VitePress",
+  title: "VitePress",
   description: "Just playing around.",
-  base: "/vitepress-action-demo/",
+  cleanUrls: true,
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    search: {
+      provider: 'local'
+    },
+    outline: 'deep',
+    nav: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '开始',
+        link: '/repo/'
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Start',
+        link: '/repo/',
+      },
+      {
+        text: '目录1',
+        items: [
+          {
+            text: '页面1',
+            link: '/repo/page1'
+          },
+          {
+            text: '页面2',
+            link: '/repo/page2'
+          }
+        ]
+      }
+    ],
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/htnanako/'
+      }
+    ]
+  },
 };
