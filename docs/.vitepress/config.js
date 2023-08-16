e07e3c47b55060c2dd86f323353c84b0d599976f
Alano-i <522081732@qq.com>
2023-08-16 09:22:53 +0800

    修改样式

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -21,6 +21,12 @@ module.exports = {
     ['style', {}, `
     :root {
       --vp-c-brand: #10b981;
+      --vp-c-text-code: #34cb94;
+      --vp-c-mute: #2e2e2e;
+      --vp-button-brand-bg: #059669;
+      --vp-button-brand-hover-bg: #047857;
+      --vp-button-brand-hover-border: #059669;
+      --vp-button-brand-active-border: #34d399;
     }
     `]
   ],
