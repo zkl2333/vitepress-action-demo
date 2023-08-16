b8cef09a01be718afe9e9b10f666ebe5227b4664
Alano-i <522081732@qq.com>
2023-08-16 10:04:33 +0800

    指南

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,6 +18,18 @@ module.exports = {
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }],
+    // ['style', {}, `
+    // :root {
+    //   --vp-c-brand: #10b981;
+    //   --vp-c-text-code: #34cb94;
+    //   --vp-c-mute: #2e2e2e;
+    //   --vp-button-brand-bg: #059669;
+    //   --vp-button-brand-hover-bg: #047857;
+    //   --vp-button-brand-hover-border: #059669;
+    //   --vp-button-brand-active-bg: #047051;
+    //   --vp-button-brand-active-border: #34d399;
+    // }
+    // `]
   ],
   themeConfig: {
     logo: { src: '/vitepress-logo-mini.svg', width: 24, height: 24 },
@@ -48,7 +60,12 @@ function nav() {
   return [
     { text: "首页", link: "/" },
     { text: "文档", link: "/contents/" },
-    { text: "Guide", link: "https://vitepress.dev/guide/markdown" },
+    { text: "Guide", link: "https://vitepress.dev/guide/markdown",
+      items: [
+        { text: 'VitePress中文网', link: 'https://vitejs.cn/vitepress/guide/markdown.html' },
+        { text: '官方文档', link: 'https://vitepress.dev/guide/markdown' },
+      ]
+    },
   ]
 }
 
